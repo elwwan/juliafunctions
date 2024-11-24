05cf007ac54a86f9ffd7fc28975feff7bd0e4979
Elwan <87530085+elwwan@users.noreply.github.com>
2024-11-24 23:31:31 +0100

    Add files via upload

---
+++ regression et graphique.jl
@@ -0,0 +1,36 @@
+using CSV
+using DataFrames
+using CairoMakie
+@time import Statistics.mean
+@time import Statistics
+@time 11+1
+function calculate_regression_line(x_data, y_data)
+  x̄, ȳ = mean(x_data), mean(y_data)
+  slope = sum([(x - x̄) * (y - ȳ) for (x, y) in zip(x_data, y_data)]) / sum([(x - x̄)^2 for x in x_data])
+  intercept = ȳ - slope * x̄
+  return slope, intercept
+end
+
+
+function create_graph(file_path::String, output_path::String, x_column_index::Int = 2, y_column_index::Int = 3; title="", xlabel="", ylabel="", subtitle="")
+    data_frame = DataFrame(CSV.File(file_path))
+    x_data = data_frame[:, x_column_index]
+    y_data = data_frame[:, y_column_index]
+    data_points = Point2f.(x_data, y_data)
+    
+    slope, intercept = calculate_regression_line(x_data, y_data)
+    
+    fig = Figure()
+    axis = Axis(fig[1, 1],
+        xlabel = xlabel,
+        ylabel = ylabel,
+        title = title,
+        subtitle = subtitle,
+        xticks = 0:50:600,
+        yticks = 1000:500:5000)
+    
+    scatter!(axis, data_points)
+    ablines!(axis, intercept, slope, label = "Droite de régression")
+    axislegend(axis, position = :rb)
+    CairoMakie.save(output_path, fig)
+end
